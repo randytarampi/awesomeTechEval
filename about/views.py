188d5026727778791ef120f2fd9235be1bc7a63e
Randy Tarampi <randy.tarampi@gmail.com>
2012-10-22 14:14:52 -0700

    Added in an About app and fleshed out the index page. Some other tidying up on a few other template pages as well.

---
+++ about/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from polls.models import Choice, Poll
+
+# Create your views here.
+def index(request):
+    return render_to_response('about.html')
